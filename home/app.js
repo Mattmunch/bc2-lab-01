2c7b4a1486ac26668cc4d68499a7089f196d2211
Matt Munch <mattmunch94@gmail.com>
2019-10-29 10:59:23 -0700

    items are filtering properly

--- home/app.js
+++ home/app.js
@@ -2,9 +2,35 @@ import images from '../data/images.js';
 import htmlToDOM from '../utils/html-to-DOM.js';
 import renderImageItem from './render-image-item.js';
 const list = document.querySelector('.images');
+const imageTypeFilter = document.querySelector('.image-type-filter');
 
-images.forEach(image => {
-    const html = renderImageItem(image);
-    const dom = htmlToDOM(html);
-    list.appendChild(dom);
+imageTypeFilter.addEventListener('change', () => {
+    const filter = imageTypeFilter.value;
+    let filteredImages = null;
+
+    if (!filter) {
+        filteredImages = images;
+    } else {
+        filteredImages = images.filter(image => {
+            return image.keyword === filter;
+        });
+    }
+
+    renderImages(filteredImages);
 });
+
+renderImages(images);
+
+
+function renderImages(imagesToRender){
+
+    while (list.lastElementChild) {
+        list.lastElementChild.remove();
+    }
+    imagesToRender.forEach(image => {
+        const html = renderImageItem(image);
+        const dom = htmlToDOM(html);
+        list.appendChild(dom);
+    });
+}
+
